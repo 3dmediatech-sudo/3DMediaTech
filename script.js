92401de3ad184c16bea58480b72070e846b31991
3dmediatech-sudo <3dmediatech@gmail.com>
2026-07-03 19:31:22 -0400

    Add files via upload
    3D updated v6

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-if (menuToggle && navLinks) {
-  menuToggle.addEventListener('click', () => navLinks.classList.toggle('open'));
-}
-
 const loadButton = document.getElementById('loadVideo');
 if (loadButton) {
-  loadButton.addEventListener('click', function () {
-    const input = document.getElementById('videoLink');
-    const box = document.getElementById('videoBox');
-    const link = input.value.trim();
+  loadButton.addEventListener('click', () => {
+    const link = document.getElementById('videoLink').value.trim();
     let videoId = '';
-
-    if (link.includes('youtube.com/watch?v=')) {
-      videoId = link.split('v=')[1].split('&')[0];
-    } else if (link.includes('youtu.be/')) {
-      videoId = link.split('youtu.be/')[1].split('?')[0];
-    } else {
-      alert('Only approved media links are supported in this demo.');
-      return;
-    }
-
-    box.innerHTML = `<iframe src="https://www.youtube.com/embed/${videoId}" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    if (link.includes('youtube.com/watch?v=')) videoId = link.split('v=')[1].split('&')[0];
+    else if (link.includes('youtu.be/')) videoId = link.split('youtu.be/')[1].split('?')[0];
+    else { alert('Only approved media links are supported in this demo.'); return; }
+    document.getElementById('videoBox').innerHTML = `<iframe width="900" height="506" src="https://www.youtube.com/embed/${videoId}" title="3D Media Demo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
   });
 }
